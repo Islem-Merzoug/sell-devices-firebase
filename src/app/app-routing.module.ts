dc7082fb41651c2b5a1154e16fd6f9832e50ba07
Islem Merzoug <i.merzoug16@gmail.com>
2020-12-15 14:30:03 +0100

    A small application to sell all types of drives on line, using a Firebase authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {AuthGuardService} from "./services/auth-guard.service";
+import {AppareilViewComponent} from "./appareil-view/appareil-view.component";
+import {SingleAppareilComponent} from "./single-appareil/single-appareil.component";
+import {EditAppareilComponent} from "./edit-appareil/edit-appareil.component";
+import {AuthComponent} from "./auth/auth.component";
+import {UserListComponent} from "./user-list/user-list.component";
+import {NewUserComponent} from "./new-user/new-user.component";
+import {FourOhFourComponent} from "./four-oh-four/four-oh-four.component";
 
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'appareils', canActivate: [AuthGuardService], component: AppareilViewComponent },
+  { path: 'appareils/:id', canActivate: [AuthGuardService], component: SingleAppareilComponent },
+  { path: 'edit', canActivate: [AuthGuardService], component: EditAppareilComponent },
+  { path: 'auth', component: AuthComponent },
+  { path: 'users', component: UserListComponent },
+  { path: 'new-user', component: NewUserComponent },
+  { path: '', canActivate: [AuthGuardService], component: AppareilViewComponent },
+  { path: 'not-found', component: FourOhFourComponent },
+  { path: '**', redirectTo: 'not-found' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(appRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
